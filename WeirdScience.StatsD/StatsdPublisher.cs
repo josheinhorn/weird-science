f472d441c5000d20d55650acb0d3d4a5e51766c6
Ei8htSe7en <josh.einhorn@gmail.com>
2015-09-01 08:40:16 -0400

    Switched to regex :runner:

--- WeirdScience.StatsD/StatsdPublisher.cs
+++ WeirdScience.StatsD/StatsdPublisher.cs
@@ -1,48 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using StatsdClient;
+﻿using StatsdClient;
+using System.Text.RegularExpressions;
 
 namespace WeirdScience.StatsD
 {
     public class StatsdPublisher : ISciencePublisher
     {
+        #region Private Fields
+
+        private static readonly Regex splitterPattern = new Regex(@"(:|\|)");
+        private static readonly Regex statsdPattern = new Regex(@"^[^:\|]+:(\d+\|(g|c|ms)|[^:\|]+\|s)$");
         private string hostName;
         private int port;
         private string prefix;
 
+        #endregion Private Fields
+
+        #region Public Constructors
+
         public StatsdPublisher(string hostName, int port)
             : this(hostName, port, string.Empty)
         { }
+
         public StatsdPublisher(string hostName, int port, string prefix)
         {
             this.hostName = hostName;
             this.port = port;
             this.prefix = prefix;
         }
+
+        #endregion Public Constructors
+
         //public StatsDPublisher(string hostName, int port, string prefix, IStopWatchFactory swFactory)
         //{
 
+        #region Public Methods
+
         //}
         /// <summary>
-        /// Sends properly formatted StatsD messages prefixed by "[Experiment Name]." and 
-        /// suffixed by ".[Current Name].[Current Step]"
+        /// Sends properly formatted StatsD messages prefixed by "[Experiment Name]." and suffixed
+        /// by ".[Current Name].[Current Step]"
         /// </summary>
         /// <example>
-        /// state = new ExperimentState { Name = "Candidate 1", ExperimentName = "Science!", 
-        ///     CurrentStep = Operations.OnMismatch };
-        /// Publish("gaugor:333|g", state) -->
-        /// Sends StatsD with: Name: "Science!.gaugor.Candidate_1.OnMismatch", Value: 333, Type: Gauge
+        /// state = new ExperimentState { Name = "Candidate 1", ExperimentName = "Science!",
+        /// CurrentStep = Operations.OnMismatch }; Publish("gaugor:333|g", state) --&gt; Sends
+        /// StatsD with: Name: "Science!.gaugor.Candidate_1.OnMismatch", Value: 333, Type: Gauge
         /// </example>
-        /// <remarks>see https://github.com/etsy/statsd/blob/master/docs/metric_types.md for formatting</remarks>
+        /// <remarks>
+        /// see https://github.com/etsy/statsd/blob/master/docs/metric_types.md for formatting
+        /// </remarks>
         /// <param name="message"></param>
         /// <param name="state"></param>
         public void Publish(string message, IExperimentState state)
         {
-            // Do nothing for now
-            // gaugor:333|g
+            // Do nothing for now gaugor:333|g
             if (!string.IsNullOrEmpty(message))
             {
                 using (var udp = new StatsdUDP(hostName, port))
@@ -70,6 +80,10 @@ namespace WeirdScience.StatsD
             }
         }
 
+        #endregion Public Methods
+
+        #region Private Methods
+
         private static void AddObservationStats<T>(string experimentName, IObservation<T> observation, Statsd statsd)
         {
             statsd.Add<Statsd.Timing>(string.Format("{0}.{1}.Results.Microseconds", experimentName, observation.Name),
@@ -79,52 +93,58 @@ namespace WeirdScience.StatsD
                 // Gauge, Meter, or Count -- use Count for now, gives us # of mismatches / second
                 statsd.Add<Statsd.Counting>(string.Format("{0}.{1}.Results.Mismatches", experimentName, observation.Name)
                     , 1);
-            }   
+            }
         }
 
         private static bool TrySendMessage(string message, string prefix, string suffix, Statsd statsd)
         {
             bool result = false;
-            var arr = message.Split(':');
-            if (arr.Length == 2)
+            if (statsdPattern.IsMatch(message)) //Shortcut if it's not a valid string
             {
-                var name = string.Format("{0}.{1}.{2}", prefix, arr[0], suffix);
-                arr = arr[1].Split('|');
-                int iNum;
-                double dNum;
-                if (arr.Length == 2)
+                var arr = splitterPattern.Split(message);
+                if (arr.Length == 5)
                 {
-                    switch (arr[1])
+                    var name = string.Format("{0}.{1}.{2}", prefix, arr[0], suffix);
+                    var value = arr[2];
+                    var type = arr[4];
+                    int iNum;
+                    double dNum;
+                    switch (type)
                     {
                         case "g":
-                            if (double.TryParse(arr[0], out dNum))
+                            if (double.TryParse(value, out dNum))
                             {
                                 statsd.Send<Statsd.Gauge>(name, dNum);
                                 result = true;
                             }
                             break;
+
                         case "s":
-                            statsd.Send<Statsd.Set>(name, arr[0]);
+                            statsd.Send<Statsd.Set>(name, value);
                             result = true;
                             break;
+
                         case "ms":
-                            if (int.TryParse(arr[0], out iNum))
+                            if (int.TryParse(value, out iNum))
                             {
                                 statsd.Send<Statsd.Timing>(name, iNum);
                                 result = true;
                             }
                             break;
+
                         case "c":
-                            if (int.TryParse(arr[0], out iNum))
+                            if (int.TryParse(value, out iNum))
                             {
                                 statsd.Send<Statsd.Counting>(name, iNum);
                                 result = true;
                             }
                             break;
                     }
-                }                
+                }
             }
             return result;
         }
+
+        #endregion Private Methods
     }
 }
